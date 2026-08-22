c2e2057395e9186e28c01068d9820f921e7c27d9
Kevin Daly <krd313@gmail.com>
2026-08-22 16:52:08 +1000

    bootsrap1_

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,9 @@
-<?php
+ <?php
 
-use App\Providers\AppServiceProvider;
+ use App\Providers\AppServiceProvider;
+-use App\Providers\PailServiceProvider;
 
-return [
-    AppServiceProvider::class,
-];
+ return [
+     AppServiceProvider::class,
+-    Laravel\Pail\PailServiceProvider::class,
+ ];
